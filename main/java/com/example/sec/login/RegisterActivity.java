96bc611cbc49024349c37809347c67c5d82a19ef
조은채 <jnidns7@gmail.com>
2019-01-22 19:20:14 +0900

    no message

---
+++ main/java/com/example/sec/login/RegisterActivity.java
@@ -0,0 +1,21 @@
+package com.example.sec.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private ArrayAdapter adapter;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        spinner = (Spinner) findViewById(R.id.majorSpinner);
+
+    }
+}
